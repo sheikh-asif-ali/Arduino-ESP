34736a54a1e4f8f818a255440b669517b205f3a1
Sheikh Asif Ali <asifali9329@gmail.com>
2023-01-18 12:01:43 +0530

    Update USS_Threshold.ino

--- USS_Threshold/USS_Threshold.ino
+++ USS_Threshold/USS_Threshold.ino
@@ -2,27 +2,25 @@
 int const trigPin = 10;
 int const echoPin = 9;
 int const buzzPin = 2;
-void setup()
-{
+
+void setup(){
 pinMode(trigPin, OUTPUT); // trig pin will have pulses output
 pinMode(echoPin, INPUT); // echo pin should be input to get pulse width
 pinMode(buzzPin, OUTPUT); // buzz pin is output to control buzzering
 }
-void loop()
-{
-// Duration will be the input pulse width and distance will be the distance to the obstacle in centimeters
-int duration, distance;
-// Output pulse with 1ms width on trigPin
-digitalWrite(trigPin, HIGH);
+
+void loop(){
+
+int duration, distance; // Duration will be the input pulse width and distance will be the distance to the obstacle in centimeters
+digitalWrite(trigPin, HIGH);  // Output pulse with 1ms width on trigPin
 delay(1);
 digitalWrite(trigPin, LOW);
-// Measure the pulse input in echo pin
-duration = pulseIn(echoPin, HIGH);
-// Distance is half the duration devided by 29.1 (from datasheet)
-distance = (duration/2) / 29.1;
-// if distance less than 0.5 meter and more than 0 (0 or less means over range)
-if (distance <= 50 && distance >= 0) 
-{// Buzz
+
+duration = pulseIn(echoPin, HIGH);  // Measure the pulse input in echo pin
+distance = (duration/2) / 29.1; // Distance is half the duration devided by 29.1 (from datasheet)
+if (distance <= 50 && distance >= 0)  // if distance less than 0.5 meter and more than 0 (0 or less means over range)
+{
+  // Turn Buzzer on for 5 times
   for(int i=0;i<5;i++)
   {
     digitalWrite(buzzPin, HIGH);
@@ -32,9 +30,8 @@ if (distance <= 50 && distance >= 0)
   }
 } 
 else {
-// Don't buzz
-digitalWrite(buzzPin, LOW);
+
+digitalWrite(buzzPin, LOW); // Don't buzz
 }
-// Waiting 60 ms won't hurt any one
-delay(60);
+delay(600);  // Repeat whole process after 6secs
 }

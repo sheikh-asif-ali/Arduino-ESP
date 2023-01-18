857db2ef83b07c5a9afdf7d4e7e86d6af7247826
Sheikh Asif Ali <asifali9329@gmail.com>
2023-01-18 11:56:17 +0530

    Update PIR_Sensor_Arudino.ino

--- PIR_Sensor_Arudino/PIR_Sensor_Arudino.ino
+++ PIR_Sensor_Arudino/PIR_Sensor_Arudino.ino
@@ -1,15 +1,7 @@
-/*
- * Created by ArduinoGetStarted.com
- *
- * This example code is in the public domain
- *
- * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-motion-sensor-led
- */
-
-const int MOTION_SENSOR_PIN = 3;   // Arduino pin connected to the OUTPUT pin of motion sensor
+const int MOTION_SENSOR_PIN = 3;    // Arduino pin connected to the OUTPUT pin of motion sensor
 const int LED_PIN           = 13;   // Arduino pin connected to LED's pin
-int motionStateCurrent      = LOW; // current  state of motion sensor's pin
-int motionStatePrevious     = LOW; // previous state of motion sensor's pin
+int motionStateCurrent      = LOW;  // current  state of motion sensor's pin
+int motionStatePrevious     = LOW;  // previous state of motion sensor's pin
 
 void setup() {
   Serial.begin(9600);                // initialize serial
